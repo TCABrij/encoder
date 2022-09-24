571e11ba8deec632a2adbf89fd79d4227a26b333
Brij Kishor <exreply7878@gmail.com>
2022-09-24 11:21:09 +0530

    input validation

--- decoder-min/decoder.js
+++ decoder-min/decoder.js
@@ -1,9 +1,55 @@
+const form = document.querySelector('form')
+const inputField = document.querySelector('#input')
+
+form.addEventListener('submit', decode)
+
+
+function decode(){
+  
+  // input field sholdn't be empty
+  if(empty(inputField)){
+    alert("Atleast one input required! 😐")
+    return
+  }
+  
+  // Validate only numbers allowed
+  // if(!alphaNumericInput(inputField.value)){
+  //   alert("Enter Encoded code only 🥺")
+  //   return
+  // }
+
+  console.log(alphaNumericInput(inputField.value))
+
+  
+}
+
+
+
+function empty(input){
+  return input.value === '' ? true : false
+}
+
+function alphaNumericInput(input){
+  let small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+  let capital = small.map((letter) => letter.toUpperCase())
+  
+  small.forEach( letter => {
+    for(let i=0; i<input.length; i++){
+      if(letter == input[i])
+        break;
+    }
+
+    return true
+  })
+  
+}
+
 function showOutput() {
-    mnemonicsCode.forEach((code) => {
+    mnemonicsCode.forEach((output) => {
       const para = document.createElement("p");
-      para.textContent = code;
+      para.textContent = output;
       para.classList.add("word-style");
-      outputDiv.append(para);
+      document.querySelector('.output-div').append(para);
     });
     mnemonicsCode = [];
   }
